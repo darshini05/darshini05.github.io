4680095112464c559de43f0093d954aa1a8731f9
darshini05 <88848506+darshini05@users.noreply.github.com>
2021-08-12 12:48:59 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function hidePayRateInput() {
+    document.getElementById('payratelabel').style.display = 'none';
+    document.getElementById('payrate').style.display = 'none';
+}
+
+function showPayRateInput() {
+    document.getElementById('payratelabel').style.display = 'block';
+    document.getElementById('payrate').style.display = 'block';
+}
